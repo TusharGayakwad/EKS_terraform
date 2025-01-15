fc061e844736a7a65d1e0a1b2155de64113a431f
Tushar Gayakwad <tushargayakwad29@gmail.com>
2025-01-15 20:05:59 +0530

    Update a vpc file.

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,31 +1,3 @@
-# resource "aws_vpc" "eks_demo" {
-#   cidr_block       = var.vpc_cidr
-#   instance_tenancy = var.instance_tenancy
-
-#   tags = {
-#     Name = var.tag_env
-#   }
-# }
-
-# resource "aws_subnet" "public_subnet" {
-#   vpc_id     = aws_vpc.eks_demo.id
-#   cidr_block = var.public_subnet_cidr
-
-#   tags = {
-#     Name = var.tag_env
-#   }
-# }
-# resource "aws_subnet" "private_subnet" {
-#   vpc_id     = aws_vpc.eks_demo.id
-#   cidr_block = var.private_subnet_cidr
-
-#   tags = {
-#     Name = var.tag_env
-#   }
-# }
-
-
-# VPC Creation
 resource "aws_vpc" "eks_demo" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = var.instance_tenancy
